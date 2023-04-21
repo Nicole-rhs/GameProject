72b50fb0cf4b287f4af6c08eeeacbf72ee40c825
Nicole-rhs <1281200128@k12.hi.us>
2023-04-21 10:08:46 -1000

    fixed code spacing

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function start() {
 
 //Player
 function player() {
-  this.x = w / 2;
+  this.x = w/2;
   this.y = 0;
   this.gravity = 0.5;
   this.lift = -15;
@@ -42,12 +42,12 @@ function player() {
       this.x = w;
     }
     if (this.y <= -33) {
-      this.y = -2 * h;
+      this.y = -2*h;
       noLoop();
       document.getElementById("end").style.display = "block";
     }
-    if (this.y >= h + 30) {
-      this.y = 3 * h;
+    if (this.y >= h+30) {
+      this.y = 3*h;
       noLoop();
       document.getElementById("end").style.display = "block";
     }
@@ -57,7 +57,7 @@ function player() {
 //Obstacles * 7
 function blockA() {
   this.x = w + 20;
-  this.y = h / 2;
+  this.y = h/2;
   this.l = 160;
   this.w = 183;
 
@@ -70,14 +70,14 @@ function blockA() {
     //To get a new, random location
     if (this.x <= -35) {
       this.x = w + 50;
-      this.y = random(h / 4, h / 2 + h / 4);
+      this.y = random(h/4, h/2 + h/4);
     }
   }
 }
 
 function blockB() {
   this.x = w + 123;
-  this.y = h / 2 - 99;
+  this.y = h/2 - 99;
   this.l = 70;
   this.w = 63;
 
@@ -90,14 +90,14 @@ function blockB() {
     //To get a new, random location
     if (this.x <= -35) {
       this.x = w + random(20, 70);
-      this.y = random(20, h / 2);
+      this.y = random(20, h/2);
     }
   }
 }
 
 function blockC() {
-  this.x = 2 * w + 222;
-  this.y = h / 2 - 123;
+  this.x = 2*w + 222;
+  this.y = h/2 - 123;
   this.l = 111;
   this.w = 80;
 
@@ -109,15 +109,15 @@ function blockC() {
     this.x = this.x -= 5.7;
     //To get a new, random location
     if (this.x <= -35) {
-      this.x = w + random(w / 3, w / 2);
-      this.y = random(h / 2, h - 20);
+      this.x = w + random(w/3, w/2);
+      this.y = random(h/2, h-20);
     }
   }
 }
 
 function blockD() {
-  this.x = 3 * w + 333;
-  this.y = h / 2 - 200;
+  this.x = 3*w + 333;
+  this.y = h/2 - 200;
   this.l = 150;
   this.w = 150;
 
@@ -129,15 +129,15 @@ function blockD() {
     this.x = this.x -= 6;
     //To get a new, random location
     if (this.x <= -35) {
-      this.x = w + random(30, w / 2);
-      this.y = random(5, h - 20);
+      this.x = w + random(30, w/2);
+      this.y = random(5, h-20);
     }
   }
 }
 
 function blockE() {
-  this.x = 5 * w + 555;
-  this.y = h / 2 + 123;
+  this.x = 5*w + 555;
+  this.y = h/2 + 123;
   this.l = 185;
   this.w = 180;
 
@@ -149,15 +149,15 @@ function blockE() {
     this.x = this.x -= 5;
     //To get a new, random location
     if (this.x <= -35) {
-      this.x = w + random(w / 2, w);
-      this.y = random(0, h - 200);
+      this.x = w + random(w/2, w);
+      this.y = random(0, h-200);
     }
   }
 }
 
 function blockF() {
-  this.x = 7 * w + 777;
-  this.y = h / 2;
+  this.x = 7*w + 777;
+  this.y = h/2;
   this.l = 150;
   this.w = 150;
 
@@ -170,14 +170,14 @@ function blockF() {
     //To get a new, random location
     if (this.x <= -35) {
       this.x = w + random(10, 100);
-      this.y = random(0, h / 4);
+      this.y = random(0, h/4);
     }
   }
 }
 
 function blockG() {
-  this.x = 10 * w;
-  this.y = h / 4;
+  this.x = 10*w;
+  this.y = h/4;
   this.l = 200;
   this.w = 200;
 
@@ -189,8 +189,8 @@ function blockG() {
     this.x = this.x -= 6.5;
     //To get a new, random location
     if (this.x <= -35) {
-      this.x = w + random(10, w / 3);
-      this.y = random(h / 2 + h / 4, h - 20);
+      this.x = w + random(10, w/3);
+      this.y = random(h/2 + h/4, h-20);
     }
   }
 }
@@ -208,8 +208,8 @@ function pointA() {
     this.x = this.x -= 3.3;
     //To get new, random location
     if (this.x <= 0) {
-      this.x = w + random(w, 2 * w);
-      this.y = random(0, h - 10);
+      this.x = w + random(w, 2*w);
+      this.y = random(0, h-10);
     }
   }
 }
@@ -226,15 +226,15 @@ function pointB() {
     this.x = this.x -= 3.5;
     //To get new, random location
     if (this.x <= 0) {
-      this.x = w + random(w, 3 * w);
-      this.y = random(0, h / 2);
+      this.x = w + random(w, 3*w);
+      this.y = random(0, h/2);
     }
   }
 }
 
 function pointC() {
   this.x = w + 555;
-  this.y = h / 2 + 100;
+  this.y = h/2 + 100;
 
   this.show = function () {
     rect(this.x, this.y, 10, 10);
@@ -244,8 +244,8 @@ function pointC() {
     this.x = this.x -= 3.5;
     //To get new, random location
     if (this.x <= 0) {
-      this.x = w + random(2 * w, 3 * w);
-      this.y = random(h / 2, h - 10);
+      this.x = w + random(2*w, 3*w);
+      this.y = random(h/2, h-10);
     }
   }
 }
